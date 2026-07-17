7f06f1eef74e2816e4a23d23d1421b0caaaf7703
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-07-17 16:24:56 -0400

    fixed landing page

--- clientside/ui/landing.js
+++ clientside/ui/landing.js
@@ -1,16 +1,19 @@
-import { error } from "node:console"
+//import { error } from "node:console"
 
-let form = document.getElementById("form-div")
+let form = document.getElementById("user-create")
 
-form.addEventListener("submit", () => {
-	console.log(form.children[1].value)
-	console.log(form.children[3].value)
-	// fetch(`/createUser/${form.children[1].value}/${form.children[3].value}`).then((res) => {
-	// 	if(res.status = 201) {
-	// 		window.location = "/main.html"
-	// 	}
-	// 	else {
-	// 		window.location = "/error.html"
-	// 	}
-	// })
+form.addEventListener("submit", (event) => {
+	event.preventDefault()
+	let v1 = form.children[0].children[1].value
+	let v2 = form.children[0].children[3].value
+	console.log(v1)
+	console.log(v2)
+	fetch(`${window.location}/createUser/${v1}/${v2}`).then((res) => {
+		if(res.status = 201) {
+			window.location = "/main.html"
+		}
+		else {
+			window.location = "/error.html"
+		}
+	})
 })
