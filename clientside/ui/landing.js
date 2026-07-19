f752d83b5c30387c914cd6f40cf7c4d71cae49dc
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-07-18 21:48:06 -0400

    removed unnecessary logs

--- clientside/ui/landing.js
+++ clientside/ui/landing.js
@@ -6,9 +6,6 @@ form.addEventListener("submit", (event) => {
 	event.preventDefault()
 	let v1 = form.children[0].children[1].value
 	let v2 = form.children[0].children[3].value
-	console.log(v1)
-	console.log(v2)
-	console.log(window.location)
 	fetch(`/createUser/${v1}/${v2}`).then((res) => {
 		console.log(res.status)
 		if(res.status = 201) {
