d64cb2739de78ae428dd2187ca8ecb63cceb3ed9
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-09-21 18:54:45 -0400

    removed some unnecessary console logs

--- preload.js
+++ preload.js
@@ -2,9 +2,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("signal",{
-	start: (val) => {
-		console.log("sending start request")
-		ipcRenderer.send("start", val)
-		console.log("request sent")
-	}
+	start: (val) => ipcRenderer.send("start", val)
 })
